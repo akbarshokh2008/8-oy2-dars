94d9954a6dc6f024a2f7dbb8408775cdfe81d743
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-11-12 19:28:47 +0500

    tugri

--- src/assets/pages/Masala.tsx
+++ src/assets/pages/Masala.tsx
@@ -1,104 +1,104 @@
-function Masala() {
-  interface Person {
-    name: string;
-    age: number;
-    isStudent: boolean;
-  }
-  interface Book {
-    title: string;
-    auther: string;
-    publishedYear: number;
-  }
+// function Masala() {
+//   interface Person {
+//     name: string;
+//     age: number;
+//     isStudent: boolean;
+//   }
+//   interface Book {
+//     title: string;
+//     auther: string;
+//     publishedYear: number;
+//   }
 
-  const Kitob: Book = {
-    title: 'The Great Gatsby',
-    auther: 'F. Scott Fitzgerald',
-    publishedYear: 1925,
-  };
+//   const Kitob: Book = {
+//     title: 'The Great Gatsby',
+//     auther: 'F. Scott Fitzgerald',
+//     publishedYear: 1925,
+//   };
 
-  const student: Person = {
-    name: 'John Doe',
-    age: 25,
-    isStudent: false,
-  };
+//   const student: Person = {
+//     name: 'John Doe',
+//     age: 25,
+//     isStudent: false,
+//   };
 
-  const Sonlar: number[] = [1, 2, 3, 4, 5];
+//   const Sonlar: number[] = [1, 2, 3, 4, 5];
 
-  function Yigindi(arr: number[]): number {
-    let javob: number = 0;
-    for (let i = 0; i < arr.length; i++) {
-      javob += arr[i];
-    }
-    return javob;
-  }
+//   function Yigindi(arr: number[]): number {
+//     let javob: number = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//       javob += arr[i];
+//     }
+//     return javob;
+//   }
 
-  const Meva: string[] = ['apple', 'banana', 'cherry'];
+//   const Meva: string[] = ['apple', 'banana', 'cherry'];
 
-  function Mevalar(mev: string[]): string[] {
-    let javob: string[] = [];
-    mev.forEach((value) => {
-      const KattaHarf = value.slice(0, 1).toUpperCase() + value.slice(1);
-      javob.push(KattaHarf);
-    });
-    return javob;
-  }
+//   function Mevalar(mev: string[]): string[] {
+//     let javob: string[] = [];
+//     mev.forEach((value) => {
+//       const KattaHarf = value.slice(0, 1).toUpperCase() + value.slice(1);
+//       javob.push(KattaHarf);
+//     });
+//     return javob;
+//   }
 
-  type StudentRecord = [string, number, boolean];
-  let Student: StudentRecord = ['Alica', 22, true];
+//   type StudentRecord = [string, number, boolean];
+//   let Student: StudentRecord = ['Alica', 22, true];
 
-  type Coordinate = [number, number];
-  let Kordinata: Coordinate = [10, 20];
+//   type Coordinate = [number, number];
+//   let Kordinata: Coordinate = [10, 20];
 
-  let Nimadur: any;
-  Nimadur = 'Salom';
-  Nimadur = 12;
-  Nimadur = true;
+//   let Nimadur: any;
+//   Nimadur = 'Salom';
+//   Nimadur = 12;
+//   Nimadur = true;
 
-  let Car: any[] = [];
-  Car.push('Damas');
-  Car.push(2018);
-  Car.push(false);
+//   let Car: any[] = [];
+//   Car.push('Damas');
+//   Car.push(2018);
+//   Car.push(false);
 
-  let qiymat: unknown;
+//   let qiymat: unknown;
 
-  qiymat = 99;
-  qiymat = 'Hello';
-  qiymat = true;
-  qiymat = { name: 'Akbarjon', age: 17 };
+//   qiymat = 99;
+//   qiymat = 'Hello';
+//   qiymat = true;
+//   qiymat = { name: 'Akbarjon', age: 17 };
 
-  function isString(value: unknown): boolean {
-    return typeof value === 'string';
-  }
+//   function isString(value: unknown): boolean {
+//     return typeof value === 'string';
+//   }
 
-  let value: unknown = 'Hello, world!';
+//   let value: unknown = 'Hello, world!';
 
-  if (isString(value)) {
-    console.log(value.toUpperCase());
-  } else {
-    console.log('Bu string emas');
-  }
+//   if (isString(value)) {
+//     console.log(value.toUpperCase());
+//   } else {
+//     console.log('Bu string emas');
+//   }
 
-  // Never
-  function throwError(): never {
-    throw new Error('Xatolik yuz berdi!');
-  }
+//   // Never
+//   function throwError(): never {
+//     throw new Error('Xatolik yuz berdi!');
+//   }
 
-  // throwError();
+//   // throwError();
 
-  function cheksiz(): never {
-    while (true) {}
-  }
+//   function cheksiz(): never {
+//     while (true) {}
+//   }
 
-  // cheksiz();
+//   // cheksiz();
 
-  // FUNCSIYA
-  function hisoblash(a: number, b: number): number {
-    return a + b;
-  }
+//   // FUNCSIYA
+//   function hisoblash(a: number, b: number): number {
+//     return a + b;
+//   }
 
-  console.log(hisoblash(5, 3));
+//   console.log(hisoblash(5, 3));
 
-  return <div>Masala</div>;
-}
+//   return <div>Masala</div>;
+// }
 
-export default Masala;
+// export default Masala;
